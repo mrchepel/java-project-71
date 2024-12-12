6802670e40ca852ccfcac087efde6a50a377e030
Aleksey Shevchuk <aleks.shev@gmail.com>
2024-12-12 15:35:22 +0300

    сonfigured gradle
    configured gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
 }
 
 tasks.test {
