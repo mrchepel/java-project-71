5a38380bda53f837cbd8ec4e2bbc22fcb8baf273
Aleksey Shevchuk <aleks.shev@gmail.com>
2024-12-19 13:45:42 +0300

    configured codeclimate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     java
     application
@@ -21,8 +24,15 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
 }
 
 application {
     mainClass.set("hexlet.code.App")
 }
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
