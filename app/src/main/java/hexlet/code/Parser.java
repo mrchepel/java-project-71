667cce7fb9347150fa107dffc34de4042d485f7d
Aleksey Shevchuk <aleks.shev@gmail.com>
2024-12-20 20:31:16 +0300

    refactor parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -5,18 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
-public class Parser {
-    private static Path getPath(String path) throws Exception {
-        Path filePath = Paths.get(path).toAbsolutePath().normalize();
-        if (!Files.exists(filePath)) {
-            throw new Exception("File '" + filePath + "' does not exist");
-        }
-        return filePath;
-    }
+import static hexlet.code.Utils.getPath;
 
+public class Parser {
     public static Map<String, Object> parse(String filePath) throws Exception {
         Path normalizePath = getPath(filePath);
         String content = new String(Files.readAllBytes(normalizePath));
---
+++ app/src/main/java/hexlet/code/Utils.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Utils {
+    public static Path getPath(String path) throws Exception {
+        Path filePath = Paths.get(path).toAbsolutePath().normalize();
+        if (!Files.exists(filePath)) {
+            throw new Exception("File '" + filePath + "' does not exist");
+        }
+        return filePath;
+    }
+}
