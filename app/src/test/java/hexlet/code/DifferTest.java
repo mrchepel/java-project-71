78903f8301cf07defefc2f3b94614edcc6fc24de
Aleksey Shevchuk <aleks.shev@gmail.com>
2024-12-20 20:30:58 +0300

    add test

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,16 +1,39 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class DifferTest {
+    private static String expected;
+
+    private static Path getFixturePath(String fileName) {
+        return Paths.get("src", "test", "resources", "fixtures", fileName)
+                .toAbsolutePath().normalize();
+    }
+
+    private static String readFixture(String fileName) throws Exception {
+        Path filePath = getFixturePath(fileName);
+        return Files.readString(filePath, StandardCharsets.UTF_8).trim();
+    }
+
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        expected = readFixture("result.txt");
+    }
 
     @Test
-    void testCompareIdenticalJson() throws Exception {
-        String json1 = "{\"key1\":\"value1\",\"key2\":\"value2\"}";
-        String json2 = "{\"key1\":\"value1\",\"key2\":\"value2\"}";
-
-        assertTrue(json1.equals(json2));
+    void testDiffer() throws Exception {
+        var path1 = getFixturePath("file1.json");
+        var path2 = getFixturePath("file2.json");
+        String actual = Differ.generate(path1.toString(), path2.toString());
+        assertEquals(expected, actual);
     }
 }
