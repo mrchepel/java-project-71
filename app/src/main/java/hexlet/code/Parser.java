99bba85ca0a14ad9d16b8d4cc7fc42847b573689
Aleksey Shevchuk <aleks.shev@gmail.com>
2024-12-22 21:11:21 +0300

    added yaml, yml file support

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,18 +2,32 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
+import static hexlet.code.Utils.getFormat;
 import static hexlet.code.Utils.getPath;
 
 public class Parser {
     public static Map<String, Object> parse(String filePath) throws Exception {
+        ObjectMapper mapper = null;
         Path normalizePath = getPath(filePath);
         String content = new String(Files.readAllBytes(normalizePath));
-        ObjectMapper mapper = new ObjectMapper();
+        String format = getFormat(filePath);
+        FileType fileType = FileType.valueOf(format.toUpperCase());
+        switch (fileType) {
+            case JSON:
+                mapper = new ObjectMapper();
+                break;
+            case YAML, YML:
+                mapper = new YAMLMapper();
+                break;
+            default:
+                break;
+        }
         return mapper.readValue(content, new TypeReference<>() {
         });
     }
