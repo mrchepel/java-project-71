2baefcbbc41b538a143d4dc41e5f422abedc9b3b
Aleksey Shevchuk <aleks.shev@gmail.com>
2024-12-26 09:40:00 +0300

    refactor parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -4,30 +4,18 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Map;
-
-import static hexlet.code.Utils.getFormat;
-import static hexlet.code.Utils.getPath;
 
 public class Parser {
     public static Map<String, Object> parse(String filePath) throws Exception {
-        ObjectMapper mapper = null;
-        Path normalizePath = getPath(filePath);
-        String content = new String(Files.readAllBytes(normalizePath));
-        String format = getFormat(filePath);
+        ObjectMapper mapper;
+        String content = Utils.getTextFromFile(filePath);
+        String format = Utils.getFormat(filePath);
         FileType fileType = FileType.valueOf(format.toUpperCase());
-        switch (fileType) {
-            case JSON:
-                mapper = new ObjectMapper();
-                break;
-            case YAML, YML:
-                mapper = new YAMLMapper();
-                break;
-            default:
-                break;
-        }
+        mapper = switch (fileType) {
+            case JSON -> new ObjectMapper();
+            case YAML, YML -> new YAMLMapper();
+        };
         return mapper.readValue(content, new TypeReference<>() {
         });
     }
