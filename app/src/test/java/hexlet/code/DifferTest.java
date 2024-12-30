67ab274933609bd21a59e3a09ea069f8f6ede7b4
Aleksey Shevchuk <aleks.shev@gmail.com>
2024-12-30 21:07:13 +0300

    fix tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static hexlet.code.Utils.getTextFromFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
