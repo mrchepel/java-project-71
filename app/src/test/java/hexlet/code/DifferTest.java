9dee91da468dccec69c827f73d9f6c23125bf89c
Aleksey Shevchuk <aleks.shev@gmail.com>
2024-12-30 21:04:15 +0300

    add tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,11 +8,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static hexlet.code.Utils.getTextFromFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class DifferTest {
     private static String expected;
+    private static String resultStylish;
+    private static String resultPlain;
+    private static String resultJson;
 
     private static Path getFixturePath(String fileName) {
         return Paths.get("src", "test", "resources", "fixtures", fileName)
@@ -27,6 +31,9 @@ class DifferTest {
     @BeforeAll
     public static void beforeAll() throws Exception {
         expected = readFixture("result.txt");
+        resultStylish = readFixture("resultStylish.txt");
+        resultPlain = readFixture("resultPlain.txt");
+        resultJson = readFixture("resultJson.txt");
     }
 
     @Test
@@ -52,4 +59,30 @@ class DifferTest {
         String actual = Differ.generate(path1.toString(), path2.toString());
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testDifferJsonStylish() throws Exception {
+        var path1 = getFixturePath("file3.json");
+        var path2 = getFixturePath("file4.json");
+        String actual = Differ.generate(path1.toString(), path2.toString(), "stylish");
+        assertEquals(resultStylish, actual);
+    }
+
+    @Test
+    void testDifferJsonPlain() throws Exception {
+        var path1 = getFixturePath("file3.json");
+        var path2 = getFixturePath("file4.json");
+        String actual = Differ.generate(path1.toString(), path2.toString(), "plain");
+        assertEquals(resultPlain, actual);
+    }
+
+    @Test
+    void testDifferJsonJson() throws Exception {
+        var path1 = getFixturePath("file3.json");
+        var path2 = getFixturePath("file4.json");
+        String actual = Differ.generate(path1.toString(), path2.toString(), "json");
+        assertEquals(resultJson, actual);
+    }
+
+
 }
